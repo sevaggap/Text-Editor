6a6ab60729179b77f03a929060a78ddb93c9700c
sevaggap <sevag.gaprielian@gmail.com>
2022-03-19 20:45:39 -0400

    added dist folder to gitignore, updated the registerRoute funciton in src-sw to properly cache images, updating the service worker pathing in the index.js file, added the HtmlWebpackPlugin, InjectManifest, and WebpackPwaManifest plugins to the webpack.config.js file, additionally added CSS loaders and babel to the webpack.config.js file, finally added scripts to the packcage.json file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,49 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+      name: 'Text Editor',
+      short_name: 'Text Editor',
+      description: 'Keep track of important text!',
+      background_color: '#7eb4e2',
+      theme_color: '#7eb4e2',
+      start_url: '/',
+      publicPath: '/',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
